b220bb1fb27914d429970e89b69735d9ab2e857c
guisso <lguisso@gmail.com>
2022-06-20 20:17:20 -0300

    A simple class to refactor and reapply tests

---
+++ src/main/java/io/github/guisso/unitarytests/Conversor.java
@@ -0,0 +1,33 @@
+/* 
+ * Material didático destinado ao curso de 
+ * Programação Orientada a Objetos
+ * do Bacharelado em Ciência da Computação do IFNMG 
+ * - Câmpus Montes Claros.
+ *
+ * O uso deste material é livre e regido pela licença 
+ * Creative Commons como Atribuição-NãoComercial
+ * -CompartilhaIgual 4.0 Internacional:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package io.github.guisso.unitarytests;
+
+/**
+ * Classe Conversor
+ *
+ * @author Luis Guisso &lt;luis dot guisso at ifnmg dot edu dot br&gt;
+ * @version 0.1, 20/06/2022
+ */
+public class Conversor {
+
+    public char converter(byte valor) {
+        if (valor == 1) {
+            return 'A';
+        } else if (valor == 2) {
+            return 'B';
+        } else if (valor == 3) {
+            return 'C';
+        } else {
+            return 'D';
+        }
+    }
+}
