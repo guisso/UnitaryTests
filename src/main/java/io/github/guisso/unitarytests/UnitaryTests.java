8c28d2fea0c7b6f5635bf8c3eb29fe3f85e506e5
guisso <lguisso@gmail.com>
2022-06-20 20:11:21 -0300

    Class with main() method

---
+++ src/main/java/io/github/guisso/unitarytests/UnitaryTests.java
@@ -0,0 +1,23 @@
+/* 
+ * Material didático destinado ao curso de 
+ * Programação Orientada a Objetos
+ * do Bacharelado em Ciência da Computação do IFNMG 
+ * - Câmpus Montes Claros.
+ *
+ * O uso deste material é livre e regido pela licença 
+ * Creative Commons como Atribuição-NãoComercial
+ * -CompartilhaIgual 4.0 Internacional:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package io.github.guisso.unitarytests;
+
+/**
+ *
+ * @author Luis Guisso &lt;luis dot guisso at ifnmg dot edu dot br&gt;
+ */
+public class UnitaryTests {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
